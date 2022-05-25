f05e599cbdc27a2f1984192f80ba6fbf61de2796
Sam Arenas <72722841+SamArenas@users.noreply.github.com>
2022-05-24 23:10:50 -0500

    add rainbow color and button change

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
-let dimension = document.querySelector('.adjust');
+// The button to adjust dimensions
+// the table itself
+// the button to clear the grid
+let btnDimension = document.querySelector('.adjust');
 let table = document.querySelector('.container');
+let btnClear = document.querySelector('.clear');
 
-let loadCells = (num) =>{
-    for(i = 0; i<num; i++){
+//loads cells in the 2D drawing grid
+let loadCells = (num) => {
+    changeDimensionHeading(num);
+
+    for (i = 0; i < num; i++) {
         let row = document.createElement("div");
-        for(j = 0; j<num; j++){
-            let square = document.createElement("div");
-            row.appendChild(square);
+        for (j = 0; j < num; j++) {
+            let cell = document.createElement("div");
+            cell.style.borderWidth = "0";
+            addCellColorChanger(cell);
+            row.appendChild(cell);
         }
         table.appendChild(row);
     }
 };
 
-let removeAllChildren = (parent) =>{
-    while(parent.firstChild){
+//changes text content of size class h3
+let changeDimensionHeading = (num) => {
+    let size = document.querySelector('.size');
+    size.textContent = num + "x" + num;
+}
+
+//deletes all cells in the 2D drawing grid
+let removeAllChildren = (parent) => {
+    while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-dimension.addEventListener('click', (e) =>{
-    let val = document.querySelector('.newDimension').value;
-    if(val>0 && val<101){
+//eventlistener that allows cell to change color when hovered over
+let addCellColorChanger = (cell) => {
+    cell.addEventListener("mouseenter", () => {
+        if (cell.style.backgroundColor == "") {
+            let r = getRandomInt(0, 256);
+            let g = getRandomInt(0, 256);
+            let b = getRandomInt(0, 256);
+            cell.style.backgroundColor = "rgb( " + r + ", " + g + ", " + b + ")";
+        }
+    });
+};
+
+//random number generator from 0 to 255 
+let getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+};
+
+//event listener when amount of cells is changed via clicking the adjust button
+btnDimension.addEventListener('click', () => {
+    newDimension = prompt("Enter the dimension of the Grid", "16");
+    if (newDimension > 0 && newDimension < 101) {
         removeAllChildren(table);
-        loadCells(val);
+        loadCells(newDimension);
+    }
+    else if (newDimension < 0 || newDimension > 100) {
+        alert("Please enter a dimension value between 0 and 100");
     }
 });
 
-window.onload = loadCells(6);
+// event listener when clear button is clicked
+btnClear.addEventListener('click', () => {
+    let cells = document.querySelectorAll(".container div div");
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = "";
+    });
+});
+
+//initial call to load a 16x16 grid in the drawing board
+window.onload = loadCells(16);
